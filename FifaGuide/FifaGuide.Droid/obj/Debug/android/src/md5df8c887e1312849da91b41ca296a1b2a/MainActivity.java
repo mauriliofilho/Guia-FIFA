71869352a89f42fd3a1d6526e10a3f9d060cb11b
Maurilio Filho <mauriliofilho22@gmail.com>
2016-03-11 19:44:09 -0300

    Guia de Jogadores e rating da Fifa
    Aplicativo para testar funções da API da FIFA

---
+++ FifaGuide/FifaGuide.Droid/obj/Debug/android/src/md5df8c887e1312849da91b41ca296a1b2a/MainActivity.java
@@ -0,0 +1,46 @@
+package md5df8c887e1312849da91b41ca296a1b2a;
+
+
+public class MainActivity
+	extends md5530bd51e982e6e7b340b73e88efe666e.FormsApplicationActivity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("FifaGuide.Droid.MainActivity, FifaGuide.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("FifaGuide.Droid.MainActivity, FifaGuide.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
